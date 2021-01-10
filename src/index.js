4fcaa6955e6738335cf7051712bf6c64a1464fee
longetka <longetka@gmail.com>
2021-01-10 21:04:42 +0700

    aboutme and skills section complete

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import './styles/style.scss';
 import img from './images/pics/DenisPhoto.jpg';
+import reactImg from './images/icons/react.png';
+import nodeImg from './images/icons/nodejs.png';
+import wordpressImg from './images/icons/wordpress.png';
+import mongoDBImg from './images/icons/mongodb.png';
+import expressImg from './images/icons/express.png';
 
 document.querySelector('.imageBlock__img').src = img;
+document.querySelector('#react').src = reactImg;
+document.querySelector('#nodejs').src = nodeImg;
+document.querySelector('#wordpress').src = wordpressImg;
+document.querySelector('#mongodb').src = mongoDBImg;
+document.querySelector('#express').src = expressImg;
